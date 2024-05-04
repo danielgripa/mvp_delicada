2b6b3b815a50d699bc431340d42a9c8938055d1b
danielgripa <112982758+danielgripa@users.noreply.github.com>
2024-05-04 01:13:27 -0300

    Ajustado racional de vendas do mês anterior

--- dash/main.py
+++ dash/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
 import streamlit as st
+from datetime import datetime
 import os
 
 # Função para organizar o resumo de transferências e compras em um DataFrame
@@ -44,6 +45,15 @@ def organizar_resumo_ajuste(df_deficit_atual, df_cobertura_ok_atual):
 
     return df_resumo_ajuste, df_produtos_compra
 
+# Classificando os produtos de acordo com a Curva ABC
+def classificar_curva_abc(proporcao):
+    if proporcao <= 0.7:
+        return 'A'
+    elif proporcao <= 0.9:
+        return 'B'
+    else:
+        return 'C'
+
 # Dados de conexão com o banco de dados
 db_uri = os.environ.get("URL")
 
@@ -59,14 +69,44 @@ with engine.connect() as conn:
     rows = result.fetchall()
     df = pd.DataFrame(rows, columns=result.keys())
 
+df = df.drop_duplicates(subset=['nk_estoque'])
+
+# Convertendo 'sk_data' para tipo datetime
 df['sk_data'] = pd.to_datetime(df['sk_data'].astype(str), format='%Y%m%d')
-df_last_month = df[df['sk_data'].dt.month == (df['sk_data'].dt.month.max() - 1)]
+
+
+# Obtendo a data atual e convertendo para um período mensal
+ultimo_mes = pd.to_datetime(datetime.today()).to_period('M')
+
+
+# Filtrando dados para o mês anterior ao último mês
+df_last_month = df[df['sk_data'].dt.to_period('M') == ultimo_mes - 1]
+
+# Agrupando por 'nk_entidade' e 'nk_produto_grade' e somando as vendas
 sales_last_month = df_last_month.groupby(['nk_entidade', 'nk_produto_grade'])['venda'].sum().reset_index()
 sales_last_month.columns = ['nk_entidade', 'nk_produto_grade', 'vendas_ultimo_mes']
+
+# Mesclando de volta ao DataFrame original com base em 'nk_entidade' e 'nk_produto_grade'
 df = pd.merge(df, sales_last_month, on=['nk_entidade', 'nk_produto_grade'], how='left')
+
+# Calculando cobertura (dobro das vendas do último mês)
 df['cobertura'] = 2 * df['vendas_ultimo_mes']
-df['saldo_versus_cobertura'] = df['saldo'] - (2 * df['vendas_ultimo_mes'])
-df = df.sort_values(by=['nk_entidade', 'nk_produto_grade', 'sk_data'], ascending=[True, True, False])
+
+# Calculando o saldo versus cobertura
+df['saldo_versus_cobertura'] = df['saldo'] - df['cobertura']
+
+# Ordenando o DataFrame por vendas em ordem decrescente
+df = df.sort_values(by='vendas_ultimo_mes', ascending=False)
+
+# Calculando a proporção cumulativa das vendas
+df['proporcao_vendas_cumulativa'] = df['vendas_ultimo_mes'].cumsum() / df['vendas_ultimo_mes'].sum()
+
+df['CURVA_ABC'] = df['proporcao_vendas_cumulativa'].apply(classificar_curva_abc)
+
+# Ordenando o DataFrame
+df = df.sort_values(by=['nk_entidade', 'sk_data', 'nk_produto_grade'], ascending=[True, False, True])
+
+
 df_atual = df.drop_duplicates(subset=['nk_entidade', 'nk_produto_grade'])
 df_deficit_atual = df_atual[df_atual['saldo_versus_cobertura'] < 0]
 df_cobertura_ok_atual = df_atual[df_atual['saldo_versus_cobertura'] >= 0]
@@ -75,11 +115,16 @@ df_resumo_ajuste, df_resumo_compra = organizar_resumo_ajuste(df_deficit_atual, d
 df_resumo_ajuste = df_resumo_ajuste.sort_values(by='transferencia_total', ascending=True)
 df_resumo_compra = df_resumo_compra.sort_values(by='saldo_necessario', ascending=False)
 
-caminho_transferencias = 'resumo_ajuste.csv'
-caminho_compras = 'compras.csv'
+caminho_transferencias = 'resumo_ajuste.xlsx'
+caminho_compras = 'compras.xlsx'
+caminho_base = 'base-geral.xlsx'
 
-df_resumo_ajuste.to_csv(caminho_transferencias, index=False)
-df_resumo_compra.to_csv(caminho_compras)
+df_resumo_ajuste.to_excel(caminho_transferencias, index=False)
+df_resumo_compra.to_excel(caminho_compras)
+df_atual.to_excel(caminho_base)
+df_deficit_atual.to_excel("base_deficit.xlsx", index = False)
+df_cobertura_ok_atual.to_excel("base_excedente.xlsx", index = False)
+
 
 st.set_page_config(page_title="Ajustes de Estoque", layout="wide")
 
